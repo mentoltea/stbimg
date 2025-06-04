bb3ec03e1c962c122e83f4501191fec412eeb063
s7k-windows <benotimprotant@mail.ru>
2025-06-05 01:44:34 +0300

    Edge and non-edge variant of add_line distinguished

--- image_edit.hpp
+++ image_edit.hpp
@@ -20,7 +20,16 @@ void draw_line(Image &img,
 );
 
 template<class ColorT>
-void add_line(Image &img, 
+void add_line_edge(Image &img, 
+    float x0, float y0,
+    float x1, float y1,
+    ColorT clr,
+    float thickness,
+    float k = 0.5
+);
+
+template<class ColorT>
+void add_line_noedge(Image &img, 
     float x0, float y0,
     float x1, float y1,
     ColorT clr,
@@ -190,7 +199,7 @@ void draw_line(Image &img,
 
 
 template<class ColorT>
-void add_line(Image &img, 
+void add_line_edge(Image &img, 
     float x0, float y0,
     float x1, float y1,
     ColorT clr,
@@ -229,18 +238,18 @@ void add_line(Image &img,
     float ypxl1 = (float) ( (int) (yend));
     
     if (steep) {
-        plot_add<ColorT>(img, ypxl1, xpxl1, clr * rfpart(yend) * xgap, k);
+        plot_add<ColorT>(img, ypxl1, xpxl1, clr * 1, k);
         for (int i=1; i<w; i++) {
             plot_add<ColorT>(img, ypxl1+i, xpxl1, clr * 1, k);
         }
-        plot_add<ColorT>(img, ypxl1+ipart(w), xpxl1, clr * fpart(yend) * xgap, k);
+        plot_add<ColorT>(img, ypxl1+ipart(w), xpxl1, clr * 1, k);
     }
     else {
-        plot_add<ColorT>(img, xpxl1, ypxl1, clr * rfpart(yend) * xgap, k);
+        plot_add<ColorT>(img, xpxl1, ypxl1, clr * 1, k);
         for (int i=1; i<w; i++) {
             plot_add<ColorT>(img, xpxl1, ypxl1+i, clr * 1, k);
         }
-        plot_add<ColorT>(img, xpxl1, ypxl1+ipart(w), clr * fpart(yend) * xgap, k);
+        plot_add<ColorT>(img, xpxl1, ypxl1+ipart(w), clr * 1, k);
     }
     
     float intery = yend + gradient;
@@ -292,5 +301,104 @@ void add_line(Image &img,
         }
     }
 }
+
+template<class ColorT>
+void add_line_noedge(Image &img, 
+    float x0, float y0,
+    float x1, float y1,
+    ColorT clr,
+    float thickness,
+    float k
+) {
+    bool steep = abs(y1-y0) > abs(x1 - x0); 
+
+    if (steep) {
+        swap(x0, y0);
+        swap(x1, y1);
+    }
+    
+    if (x0 > x1) {
+        swap(x0, x1);
+        swap(y0, y1);
+    }
+    
+    float dx = x1 - x0;
+    float dy = y1 - y0;
+    float gradient;
+    
+    if (dx == 0)    gradient = 1;
+    else            gradient = dy/dx;
+    
+    float w = thickness * sqrt(1 + (gradient * gradient));
+
+    float xend = round(x0);
+    float yend = y0 - (w-1)/2 + gradient * (xend - x0);
+    float xgap = 1 - (x0 + 0.5 - xend);
+    
+    float xpxl1 = xend;
+    float ypxl1 = (float) ( (int) (yend));
+    
+    if (steep) {
+        plot_add<ColorT>(img, ypxl1, xpxl1, clr * 1, k);
+        for (int i=1; i<w; i++) {
+            plot_add<ColorT>(img, ypxl1+i, xpxl1, clr * 1, k);
+        }
+        plot_add<ColorT>(img, ypxl1+ipart(w), xpxl1, clr * 1, k);
+    }
+    else {
+        plot_add<ColorT>(img, xpxl1, ypxl1, clr * 1, k);
+        for (int i=1; i<w; i++) {
+            plot_add<ColorT>(img, xpxl1, ypxl1+i, clr * 1, k);
+        }
+        plot_add<ColorT>(img, xpxl1, ypxl1+ipart(w), clr * 1, k);
+    }
+    
+    float intery = yend + gradient;
+    
+    xend = round(x1);
+    yend = y1 - (w-1)/2 + gradient * (xend - x1);
+    xgap = 1 - (x1 + 0.5 - xend);
+    
+    float xpxl2 = xend; //this will be used in the main loop
+    float ypxl2 = (float)((int)(yend));
+    if (steep) {
+        plot_add<ColorT>(img, ypxl2, xpxl2, clr * 1, k);
+        for (int i=1; i<w; i++) {
+            plot_add<ColorT>(img, ypxl2+i, xpxl2, clr * 1, k);
+        }
+        plot_add<ColorT>(img, ypxl2+ipart(w), xpxl2, clr * 1, k);
+    }
+    else {
+        plot_add<ColorT>(img, xpxl2, ypxl2, clr * 1, k);
+        for (int i=1; i<w; i++) {
+            plot_add<ColorT>(img, xpxl2, ypxl2+i, clr * 1, k);
+        }
+        plot_add<ColorT>(img, xpxl2, ypxl2+ipart(w), clr * 1, k);
+    }
+
+    // main loop
+    if (steep) {
+        for (int x = xpxl1 + 1;  x <= xpxl2 - 1; x++) {
+            plot_add<ColorT>(img, ipart(round(intery)), x, clr * 1, k);
+            for (int i=1; i<w; i++) {
+                plot_add<ColorT>(img, ipart(round(intery))+i, x, clr * 1, k);
+            }
+            plot_add<ColorT>(img, ipart(round(intery))+ipart(w), x, clr * 1, k);
+            intery = intery + gradient;
+        }
+    }
+    else {
+        for (int x = xpxl1 + 1; x <= xpxl2 - 1; x++) {
+            plot_add<ColorT>(img, x, ipart(round(intery)), clr * 1, k);
+            for (int i=1; i<w; i++) {
+                plot_add<ColorT>(img, x, ipart(round(intery))+i, clr * 1, k);
+            }
+            plot_add<ColorT>(img, x, ipart(round(intery))+ipart(w), clr * 1, k);
+
+            intery = intery + gradient;
+        }
+    }
+}
+
 
 #endif // STB_IMAGE_WRAPPER_EDIT_IMPLEMENTATION
